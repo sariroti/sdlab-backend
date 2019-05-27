587b8d79c8c66f5e58787cdafbdf2c6ed4faeb85
Budi Erwanto <budi@call-levels.com>
2019-05-27 14:39:24 +0700

    Setup Express and Mongoose DB to Run

--- db/mongoose/server.js
+++ db/mongoose/server.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 const mongodburi = 'mongodb://127.0.0.1/sdlab';
 
-mongoose.connect(mongodburi, { useNewUrlParser: true }, err => {
-    if(err) console.log(err);
+function connect(){
+    mongoose.connect(mongodburi, { useNewUrlParser: true }, err => {
+        if(err) console.log(err);
 
-    console.log(`connected to mongoose db, uri : ${mongodburi}`);
-});
+        console.log(`connected to mongoose db, uri : ${mongodburi}`);
+    });    
+}
 
-module.exports = mongoose.connection;
+module.exports = {
+    connect,
+    db : mongoose.connection
+}
